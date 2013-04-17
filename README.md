131fec2fc76a48ad20a7a683ba2648dc0b7a7c40
Alex Kessinger <alex@mixedmedialabs.com>
2013-04-16 19:54:00 -0700

    rest of the initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ADN Social Buttons
+
+You can use the hosted version of this instead of running it your self. For more details read the blog post, or checkout the button info page on App.net.
+
+## Getting Started
+
+To start you will need npm, ruby & bundler installed on your machine. Then do the following to get a development environment running.
+
+```sh
+npm install
+bundle install
+grunt runserver
+```
---
+++ src/assets/js/adn.js
@@ -0,0 +1,57 @@
+(function (win, doc) {
+  var iframe_base = win.ADN_IFRAME_BASE || 'https://d2zh9g63fcvyrq.cloudfront.net/index.html?';
+  var nativeForEach = Array.prototype.forEach;
+  var breaker = {};
+  var each = function (obj, iterator, context) {
+    if (obj === null) {
+      return;
+    }
+    if (nativeForEach && obj.forEach === nativeForEach) {
+      obj.forEach(iterator, context);
+    } else if (obj.length === +obj.length) {
+      for (var i = 0, l = obj.length; i < l; i++) {
+        iterator.call(context, obj[i], i, obj);
+      }
+    } else {
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          iterator.call(context, obj[key], key, obj);
+        }
+      }
+    }
+  };
+
+  each(doc.querySelectorAll('.adn-button'), function (button) {
+    console.log('gots a button');
+    var properties = {};
+    var prop_query = [];
+    each(button.attributes, function (attr) {
+      var prop_name = attr.name;
+      if (prop_name.indexOf('data-') === 0) {
+        prop_name = prop_name.replace(/-/g, '_');
+        properties[prop_name] = attr.value;
+        prop_query.push(prop_name + '=' + encodeURIComponent(attr.value));
+      }
+    });
+
+    var iframe = document.createElement('iframe');
+
+    var iframe_props = {
+      width: properties.width || 100,
+      height: properties.height || 50,
+      src: iframe_base + prop_query.join('&'),
+      scrolling: 'no',
+      marginwidth: 0,
+      marginheight: 0,
+      frameborder: 0,
+      style: 'border: 0px;'
+    };
+
+    each(iframe_props, function (value, key) {
+      iframe.setAttribute(key, value);
+    });
+
+    button.parentNode.replaceChild(iframe, button);
+  });
+
+}(window, document));
